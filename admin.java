3061f28c5acee0b897fbf5b6176db89621d7c994
riya1719 <101990431+riya1719@users.noreply.github.com>
2022-05-20 16:28:01 +0530

    Add files via upload

---
+++ admin.java
@@ -0,0 +1,17 @@
+package DataStructure;
+
+public class admin {
+	int z;
+
+	public static void main(String[] args) throws Exception
+	{
+		int x = 4;
+		int y= 5;
+		float a = 5.4F;
+		char ch = 65;
+		x=6;
+	    int g = (int)a +  x;
+		System.out.println(x);
+	}
+
+}
